100995df546795d2fad942bd837a5a883b975cf0
ljy <ljy@163.com>
2024-01-04 16:14:50 +0800

    [修改] 修改flags参数

--- FileIO/testOpen.c
+++ FileIO/testOpen.c
@@ -7,7 +7,7 @@
 int main(int argc, char const *argv[])
 {
     /* 打开文件 */
-    int fd = open("./hello.txt", O_RDWR, 0644);
+    int fd = open("./hello.txt", O_RDWR | O_CREAT, 0644);
     if(fd == -1)
     {
         perror("open error\n");
